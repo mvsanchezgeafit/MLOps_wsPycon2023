0fb1536bc0bf2371f4d3107468e291b9595a5ee2
mvsanchezgeafit <mvsanchezg@eafit.edu.co>
2025-04-26 12:58:55 -0500

    Update Classifier.py

--- src/model/src/Classifier.py
+++ src/model/src/Classifier.py
@@ -1,18 +1,18 @@
-from torch import nn 
-import torch.nn.functional as F
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense, Input
+import tensorflow as tf
 
-class Classifier(nn.Module):
-  def __init__(self,
-               input_shape, 
-               hidden_layer_1, 
-               hidden_layer_2, num_classes):
-    super().__init__()
-    self.linear1 = nn.Linear(input_shape,hidden_layer_1)
-    self.linear2 = nn.Linear(hidden_layer_1,hidden_layer_2)
-    self.linear3 = nn.Linear(hidden_layer_2,num_classes)
+class Classifier(Model):
+    def __init__(self, input_shape, hidden_layer_1, hidden_layer_2, num_classes):
+        super(Classifier, self).__init__()
 
-  def forward(self, x):
-    x = F.relu(self.linear1(x))
-    x = F.relu(self.linear2(x))
-    x = self.linear3(x)  # this is the score
-    return x
+        # Definimos las capas con keras y activación ReLu
+        self.dense1 = Dense(hidden_layer_1, activation='relu')
+        self.dense2 = Dense(hidden_layer_2, activation='relu')
+        self.dense3 = Dense(num_classes)  
+
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        x = self.dense2(x)
+        x = self.dense3(x)
+        return x
